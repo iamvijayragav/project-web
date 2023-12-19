b6a8077210a769368c96e38e6e46c683e5ad0dea
B-Mani-kandan <baskarmanikandan48@gmail.com>
2023-12-19 16:19:36 +0530

    update contactus

--- src/components/pages/Careers/BackgroundSlider.js
+++ src/components/pages/Careers/BackgroundSlider.js
@@ -1,16 +1,14 @@
-import React from 'react';
-import './BackgroundSlider.css';
+import React from "react";
+import "./BackgroundSlider.css";
 
 const BackgroundSlider = () => {
   return (
     <div>
       <div className="background-slider">
-        {/* Add background images as needed */}
         <div className="background-image image1"></div>
         <div className="background-image image2"></div>
       </div>
     </div>
-
   );
 };
 
---
+++ src/components/pages/ContactUs.js
@@ -0,0 +1,116 @@
+import React from "react";
+import "./ContactUs.css";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import {
+  faAddressCard,
+  faEnvelope,
+  faInbox,
+} from "@fortawesome/free-solid-svg-icons";
+function ContactUs() {
+  return (
+    <main>
+      <section className="contact">
+        <div className="container">
+          <div className="left">
+            <div className="form-wrapper">
+              <div className="contact-heading">
+                <h1>
+                  Let's Work Together<span>.</span>
+                </h1>
+                <p className="text">
+                  Or reach us via :
+                  <a href="mailto:Foxeus@email.com">tecnico@gmail.com</a>
+                </p>
+              </div>
+
+              <form action="index.html" method="post" className="contact-form">
+                <div className="input-wrap">
+                  <input
+                    className="contact-input"
+                    name="First Name"
+                    type="text"
+                    placeholder="First Name"
+                    required
+                  />
+                  <FontAwesomeIcon className="icon" icon={faAddressCard} />
+                </div>
+
+                <div className="input-wrap">
+                  <input
+                    className="contact-input"
+                    name="Last Name"
+                    placeholder="Last Name"
+                    type="text"
+                    required
+                  />
+                  <FontAwesomeIcon className="icon" icon={faAddressCard} />
+                </div>
+
+                <div className="input-wrap w-100">
+                  <input
+                    className="contact-input"
+                    name="Email"
+                    type="email"
+                    placeholder="Email"
+                    required
+                  />
+                  <FontAwesomeIcon className="icon" icon={faEnvelope} />
+                </div>
+
+                <div className="input-wrap textarea w-100">
+                  <textarea
+                    name="Message"
+                    auto-complete="off"
+                    className="contact-input"
+                    placeholder="Message"
+                    required
+                  ></textarea>
+                  <FontAwesomeIcon className="icon" icon={faInbox} />
+                </div>
+
+                <div className="contact-buttons">
+                  <input type="submit" value="Send message" className="btn" />
+                </div>
+              </form>
+            </div>
+          </div>
+          <div className="right">
+            <div className="image-wrapper">
+              <img
+                src="https://images.unsplash.com/photo-1560885036-3adb13b6308c?q=80&w=2001&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D"
+                className="img"
+                alt="show"
+              />
+              <div className="wave-wrap">
+                <svg
+                  className="wave"
+                  viewBox="0 0 783 1536"
+                  fill="none"
+                  xmlns="http://www.w3.org/2000/svg"
+                >
+                  <path
+                    id="wave"
+                    d="M236.705 1356.18C200.542 1483.72 64.5004 1528.54 1 1535V1H770.538C793.858 63.1213 797.23 196.197 624.165 231.531C407.833 275.698 274.374 331.715 450.884 568.709C627.393 805.704 510.079 815.399 347.561 939.282C185.043 1063.17 281.908 1196.74 236.705 1356.18Z"
+                  />
+                </svg>
+              </div>
+              <svg
+                className="dashed-wave"
+                viewBox="0 0 345 877"
+                fill="none"
+                xmlns="http://www.w3.org/2000/svg"
+              >
+                <path
+                  id="dashed-wave"
+                  d="M0.5 876C25.6667 836.167 73.2 739.8 62 673C48 589.5 35.5 499.5 125.5 462C215.5 424.5 150 365 87 333.5C24 302 44 237.5 125.5 213.5C207 189.5 307 138.5 246 87C185 35.5 297 1 344.5 1"
+                />
+              </svg>
+            </div>
+          </div>
+        </div>
+      </section>
+    </main>
+  );
+}
+
+export default ContactUs;
