a6cfbf5c8162cf55ca143471b074af9196051cb7
B-Mani-kandan <baskarmanikandan48@gmail.com>
2023-12-26 10:42:30 +0530

    new

--- src/components/pages/DropDown-Pages/RiskManagement/RiskManagement.js
+++ src/components/pages/DropDown-Pages/RiskManagement/RiskManagement.js
@@ -1,9 +1,8 @@
-import React from 'react'
+import React from "react";
+import "../Engineering/Engineering.css";
 
 function RiskManagement() {
-  return (
-    <div>RiskManagement</div>
-  )
+  return <div>RiskManagement</div>;
 }
 
-export default RiskManagement
+export default RiskManagement;
